fc1f4a0d13efb6a1b99a6e4f39bfe0fdb593b69f
Ray <ray@tokenchat.io>
2019-03-25 15:45:37 +0800

    Added localization

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'error_signup' => 'Unable to signup.',
+    'error_signup_username_duplicate' => 'Username already exists.',
+
+    'error_login' => 'Unable to login.',
+    'error_login_account_not_created' => 'Nomi account not created yet.',
+    'error_login_username_not_found' => 'Unable to login. Username not found.',
+    'error_login_password_incorrect' => 'Unable to login. Password is incorrect.',
+
+    'success_logout' => 'Logout successfully.',
+
+    'error_profile_update_location' => 'Unable to update location.',
+    'error_profile_update_intro' => 'Unable to update intro.',
+    'error_profile_remove_avatar_none' => 'There is no avatar to be removed.',
+    'error_profile_remove_avatar_missing' => 'Unable to remove photo. Photo does not exist.',
+    'error_profile_remove_photo_denied' => 'Unable to remove photo. You are not authorized.',
+    'error_profile_upload_photo' => 'No photos uploaded.',
+
+    'success_profile_upload_photo' => 'Photo removed.',
+
+    'error_profile_search_available' => 'Unable to find available girls.', 
+
+    'success_booking_create' => 'Booking created successfully.',
+    'success_booking_updated' => 'Booking updated successfully.',
+    'success_booking_confirmed' => 'Booking confirmed successfully.',
+
+    'error_booking_create' => 'Unable to create booking.',
+    'error_booking_view' => 'Unable to view booking. You are not authorized.',
+    'error_booking_accept' => 'Unable to accept booking.',
+    'error_booking_fetch' => 'Unable to find booking.',
+    'error_booking_confirm' => 'Unable to confirm booking.'
+];
